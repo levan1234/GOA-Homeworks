b341c99caf1344e08605e20b6d8e9a3830b7ed73
levan1234 <levanbaliashvili981@gmail.com>
2024-04-14 00:35:39 +0400

    commit

---
+++ Day 19/homework1/homework.py
@@ -0,0 +1,5 @@
+list = [2,3,5,7,1,3]
+num = 1
+for i in list:
+    num = num * i
+print(num)
---
+++ Day 19/homework2/homework2.py
@@ -0,0 +1,6 @@
+list = [2,4,-3,-6,8]
+lists = []
+for i in list:
+    if i > 0:
+        lists.append(i)
+print(lists)
